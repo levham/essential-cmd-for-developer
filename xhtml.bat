c5c1c76b398154c8640a4f091cda74f0ebcc2929
levham <eskibirhesap@gmail.com>
2025-05-22 21:15:33 +0300

    web

---
+++ xhtml.bat
@@ -0,0 +1,36 @@
+@echo off 
+:: Rastgele dosya adı oluştur
+set "html=a%random%.html"
+
+:: HTML dosyasını oluştur ve içerik ekle
+echo.^<!-- Created by xhtml.bat --^> >%html%
+echo.^<!DOCTYPE html^> >> %html%
+echo.^<html lang="tr"^> >> %html%
+echo.^<head^> >> %html%
+echo.        ^<meta charset="UTF-8"^> >> %html%
+echo.        ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^> >> %html%
+echo.        ^<title^>Rastgele HTML Sayfası^</title^> >> %html%
+echo.        >> %html%
+echo.        ^<!-- CSS >> %html%
+echo.        ^<link rel="stylesheet" href="styles.css"^> >> %html%
+echo.        ^<style^> ^</style^> >> %html%
+echo.        --^> >> %html%
+echo.        >> %html%
+echo.        ^<!-- JS >> %html%
+echo.        ^<script src="script.js" ^>^</script^> >> %html%
+echo.        ^<script^> ^</script^> >> %html%
+echo.        --^> >> %html%
+echo.        >> %html%
+echo.^</head^> >> %html%
+echo.^<body^> >> %html%
+echo.        ^<h1^>Merhaba! stndrt HTML  ^</h1^> >> %html%
+echo.        >> %html%
+echo.        ^<!-- JS >> %html%
+echo.        ^<script src="script.js" ^>^</script^> >> %html%
+echo.        ^<script^> ^</script^> >> %html%
+echo.        --^> >> %html%
+echo.^</body^> >> %html%
+echo.^</html^> >> %html%
+
+echo "%randName% HTML dosyasi olusturuldu."
+timeout /t 2 
